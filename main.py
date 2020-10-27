db58d4e9a396dbb2810a937448d4096a79fdf33f
e185745 <e185745@ie.u-ryukyu.ac.jp>
2020-10-27 17:10:34 +0900

    implement predict_def

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ print(Y)
 import regression
 model = regression.LinearRegression()
 print(model.x)
+
+import importlib
+importlib.reload(regression)
+model = regression.LinearRegression()
+model.fit(X,Y)
+print(model.theta)
+
+importlib.reload(regression)
+model = regression.LinearRegression()
+model.fit(X,Y)
+print(model.predict(X))
